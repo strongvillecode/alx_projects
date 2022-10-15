3e074f51144d28212e732d3280ceee6518fe88da
Sarumi sodiq <strongvilleengineering@gmail.com>
2022-10-15 18:19:47 +0100

    printf command

---
+++ hello_world/printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point for the programme
+ *
+ * Return : 0 if errors, non zeros if no errors
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+		return (0);
+}
---
+++ hello_world/puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - serve as the entry point
+ *
+ * Return : By default return zero
+ */
+int main(void){
+	puts("programming is like building a multilingual puzzle one");
+	
+		return (0);
+}
